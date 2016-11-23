15170705dd97b5c055fa7c3a7f0c4112c02a7d00
cjj8110 <cjj8110@163.com>
2016-11-23 09:28:34 +0800

    fixed:composer.json

--- src/web/Route.php
+++ src/web/Route.php
@@ -1,9 +1,7 @@
 <?php
 namespace rust\web;
-
 use rust\interfaces\IRoute;
 use rust\util\Config;
-use rust\http\Request;
 
 /**
  * Class Route
@@ -26,29 +24,30 @@ class Route implements IRoute {
     public function __construct(Config $route_config) {
         $this->matchInfo = NULL;
         $this->routeInfo = [];
-        $this->paras = [];
-        $this->config = $route_config;
+        $this->paras     = [];
+        $this->config    = $route_config;
     }
 
     /**
-     * @param Request $request
+     * @param WebRequest $request
      *
      * @return bool
      */
     public function route(& $request) {
-        $result = TRUE;
-        $config = $this->config;
-        $route_info = [
+        $result            = TRUE;
+        $config            = $this->config;
+        $isControllerFront = $config->get('controllerFront');
+        $route_info        = [
             'package'    => $config->get('package'),
             'module'     => $config->get('module'),
             'controller' => $config->get('controller'),
             'action'     => $this->_getAction($request, $config->get('action')),
         ];
-        $request_uri = str_replace('\\', '/', $request->requestUri);
-        $url = trim($request_uri, '/');
+        $request_uri       = str_replace('\\', '/', $request->getUri()->getPath());
+        $url               = trim($request_uri, '/');
         if (!empty($url)) {//默认路由
             $url_info = explode('/', $url);
-            $count = count($url_info);
+            $count    = count($url_info);
             if (!$count) {
                 return FALSE;
             }
@@ -61,7 +60,7 @@ class Route implements IRoute {
             }
             $route_info['controller'] = $url_info[$index];
             $index++;
-            $action = isset($url_info[$index]) ? $url_info[$index] : '';
+            $action               = isset($url_info[$index]) ? $url_info[$index] : '';
             $route_info['action'] = $this->_getAction($request, $action);
         }
         $version = NULL;
@@ -69,42 +68,49 @@ class Route implements IRoute {
             $version = $request->getRequestVersion();
             $version = $version ? intval($version) : 1;
         }
-        $route_info['controller_class'] = $this->_getController($route_info, $version);
-        $request->setRouted($route_info);
+        $route_info['controller_class'] = $this->_getController($route_info, $isControllerFront, $version);
+        $request->setRouteInfo($route_info);
         return $result;
     }
 
     /**
-     * @param $request
-     * @param $action
+     * @param WebRequest $request
+     * @param            $action
      *
      * @return string
      */
-    private function _getAction($request, $action) {
-        $result = $request->method . ucfirst($action) . ucfirst($request->format);
+    private function _getAction(WebRequest $request, $action) {
+        $method = strtolower($request->getMethod());
+        $format = ucfirst($request->getFormat());
+        $result = $method . ucfirst($action) . $format;
         if (strpos($action, '.') !== FALSE) {
             $action_info = explode('.', $action);
             array_pop($action_info);
             $action = implode('.', $action_info);
-
-            $result = $request->method . ucfirst($action) . ucfirst($request->format);
+            $result = $method . ucfirst($action) . $format;
         }
         return $result;
     }
 
     /**
-     * @param $routed
-     * @param $version
+     * @param array $routed
+     * @param bool  $isControllerFirst
+     * @param null  $version
      *
      * @return string
      */
-    private function _getController($routed, $version) {
+    private function _getController($routed, $isControllerFirst = FALSE, $version = NULL) {
         $controller_name = isset($routed['package']) && $routed['package'] ? '\\' . $routed['package'] : '';
         if ($version) {
             $controller_name .= '\\v' . $version;
         }
-        $controller_name .= '\\controller';
+        if ($isControllerFirst) {
+            $controller_name .= '\\controller';
+        }
         $controller_name .= isset($routed['module']) && $routed['module'] ? '\\' . $routed['module'] : '';
+        if (!$isControllerFirst) {
+            $controller_name .= '\\controller';
+        }
         $controller_name .= '\\' . ucfirst($routed['controller']);
         return $controller_name;
     }
