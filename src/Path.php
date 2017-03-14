40a6e43bdbb43c0dd568ec5ebe12e2d97a1d5147
cjj8110 <cjj8110@163.com>
2017-03-14 10:36:38 +0800

    fixed: get application path

--- src/Path.php
+++ src/Path.php
@@ -1,5 +1,6 @@
 <?php
 namespace rust;
+
 use rust\fso\Dir;
 
 /**
@@ -8,71 +9,80 @@ use rust\fso\Dir;
  * @package rust
  */
 class Path {
-    const DEFAULT_CONFIG_PATH     = 'resource/config/';
-    const DEFAULT_TABLE_PATH      = 'resource/config/share/table/';
-    const DEFAULT_LOG_PATH        = 'resource/logs/';
-    const DEFAULT_CACHE_PATH      = 'resource/cache/';
-    const DEFAULT_MODEL_PATH      = 'resource/model/';
-    const DEFAULT_ROUTING_PATH    = 'resource/routing';
+    const DEFAULT_CONFIG_PATH = 'resource/config/';
+    const DEFAULT_TABLE_PATH = 'resource/config/share/table/';
+    const DEFAULT_LOG_PATH = 'resource/logs/';
+    const DEFAULT_CACHE_PATH = 'resource/cache/';
+    const DEFAULT_MODEL_PATH = 'resource/model/';
+    const DEFAULT_ROUTING_PATH = 'resource/routing';
     const DEFAULT_MIDDLEWARE_PATH = 'resource/middleware';
-    private static $rootPath       = NULL;
-    private static $configPath     = NULL;
-    private static $logPath        = NULL;
-    private static $cachePath      = NULL;
-    private static $modelPath      = NULL;
-    private static $tablePath      = NULL;
-    private static $routingPath    = NULL;
-    private static $middlewarePath = NULL;
+    const DEFAULT_APPLICATION_PATH = 'src/';
+    private static $rootPath        = NULL;
+    private static $configPath      = NULL;
+    private static $logPath         = NULL;
+    private static $cachePath       = NULL;
+    private static $modelPath       = NULL;
+    private static $tablePath       = NULL;
+    private static $routingPath     = NULL;
+    private static $middlewarePath  = NULL;
+    private static $applicationPath = NULL;
 
-    public static function init($rootPath) {
-        self::setRootPath($rootPath);
-        //
-        self::$configPath     = self::$rootPath . self::DEFAULT_CONFIG_PATH;
-        self::$logPath        = self::$rootPath . self::DEFAULT_LOG_PATH;
-        self::$modelPath      = self::$rootPath . self::DEFAULT_MODEL_PATH;
-        self::$cachePath      = self::$rootPath . self::DEFAULT_CACHE_PATH;
-        self::$tablePath      = self::$rootPath . self::DEFAULT_TABLE_PATH;
-        self::$routingPath    = self::$rootPath . self::DEFAULT_ROUTING_PATH;
-        self::$middlewarePath = self::$rootPath . self::DEFAULT_MIDDLEWARE_PATH;
+    /**
+     * @param string $rootPath
+     */
+    public static function init(string $rootPath) {
+        static::setRootPath($rootPath);
+        static::$configPath = static::$rootPath . static::DEFAULT_CONFIG_PATH;
+        static::$logPath = static::$rootPath . static::DEFAULT_LOG_PATH;
+        static::$modelPath = static::$rootPath . static::DEFAULT_MODEL_PATH;
+        static::$cachePath = static::$rootPath . static::DEFAULT_CACHE_PATH;
+        static::$tablePath = static::$rootPath . static::DEFAULT_TABLE_PATH;
+        static::$routingPath = static::$rootPath . static::DEFAULT_ROUTING_PATH;
+        static::$middlewarePath = static::$rootPath . static::DEFAULT_MIDDLEWARE_PATH;
+        static::$applicationPath = static::$rootPath . static::DEFAULT_APPLICATION_PATH;
     }
 
     public static function getRootPath() {
-        return self::$rootPath;
+        return static::$rootPath;
+    }
+
+    public static function getApplicationPath() {
+        return static::$applicationPath;
     }
 
     public static function getConfigPath() {
-        return self::$configPath;
+        return static::$configPath;
     }
 
     public static function setConfigPath($configPath) {
-        self::$configPath = $configPath;
+        static::$configPath = $configPath;
     }
 
     public static function getLogPath() {
-        return self::$logPath;
+        return static::$logPath;
     }
 
     public static function getModelPath() {
-        return self::$modelPath;
+        return static::$modelPath;
     }
 
     public static function getCachePath() {
-        return self::$cachePath;
+        return static::$cachePath;
     }
 
     public static function getTablePath() {
-        return self::$tablePath;
+        return static::$tablePath;
     }
 
     public static function getRoutingPath() {
-        return self::$routingPath;
+        return static::$routingPath;
     }
 
     public static function getMiddlewarePath() {
-        return self::$middlewarePath;
+        return static::$middlewarePath;
     }
 
     private static function setRootPath($rootPath) {
-        self::$rootPath = Dir::formatPath($rootPath);
+        static::$rootPath = Dir::formatPath($rootPath);
     }
 }
