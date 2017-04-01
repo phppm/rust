480b50341eba45fe60e10bcffde54c2511fb608b
cjj8110 <cjj8110@163.com>
2017-04-01 15:36:05 +0800

    fixed:not log CustomException

--- src/exception/handler/ExceptionHandler.php
+++ src/exception/handler/ExceptionHandler.php
@@ -4,9 +4,11 @@
  *
  * @author Filipe Dobreira <http://github.com/filp>
  */
+
 namespace rust\exception\handler;
 
 use rust\common\Config;
+use rust\exception\CustomException;
 use rust\exception\Formatter;
 use rust\exception\Inspector;
 use rust\http\Response;
@@ -37,7 +39,7 @@ class ExceptionHandler {
      */
     private $config;
 
-    public function __construct(Config $config = NULL) {
+    public function __construct(Config $config = null) {
         $this->config = $config;
     }
 
@@ -48,31 +50,33 @@ class ExceptionHandler {
         $inspector = $this->getInspector();
         $frames = $inspector->getFrames();
         $exception = $inspector->getException();
-        $code = $exception->getCode();
-        // List of variables that will be passed to the layout template.
-        $vars = [
-            "title"           => 'Whoops! There was an error.',
-            "name"            => explode("\\", $inspector->getExceptionName()),
-            "message"         => $inspector->getException()->getMessage(),
-            "code"            => $code,
-            "plain_exception" => Formatter::formatExceptionPlain($inspector),
-            "frames"          => $frames,
-            "has_frames"      => !!count($frames),
-            "handler"         => $this,
-            "handlers"        => $inspector->getHandlers(),
-            "tables"          => [
-                "INPUT DATA"            => file_get_contents('php://input'),
-                "GET Data"              => $_GET,
-                "POST Data"             => $_POST,
-                "Files"                 => $_FILES,
-                "Cookies"               => $_COOKIE,
-                "Session"               => isset($_SESSION) ? $_SESSION : [],
-                "Server/Request Data"   => $_SERVER,
-                "Environment Variables" => $_ENV,
-            ],
-        ];
         //TODO:进一步优化
-        Log::write($vars, 'error');
+        if (!$exception instanceof CustomException) {
+            $code = $exception->getCode();
+            // List of variables that will be passed to the layout template.
+            $vars = [
+                "title"           => 'Whoops! There was an error.',
+                "name"            => explode("\\", $inspector->getExceptionName()),
+                "message"         => $inspector->getException()->getMessage(),
+                "code"            => $code,
+                "plain_exception" => Formatter::formatExceptionPlain($inspector),
+                "frames"          => $frames,
+                "has_frames"      => !!count($frames),
+                "handler"         => $this,
+                "handlers"        => $inspector->getHandlers(),
+                "tables"          => [
+                    "INPUT DATA"            => file_get_contents('php://input'),
+                    "GET Data"              => $_GET,
+                    "POST Data"             => $_POST,
+                    "Files"                 => $_FILES,
+                    "Cookies"               => $_COOKIE,
+                    "Session"               => isset($_SESSION) ? $_SESSION : [],
+                    "Server/Request Data"   => $_SERVER,
+                    "Environment Variables" => $_ENV,
+                ],
+            ];
+            Log::write($vars, 'error');
+        }
         return ExceptionHandler::QUIT;
     }
 
