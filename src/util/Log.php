1d24757709cae75cf84fbf660875f1a5b350dd27
cjj8110 <cjj8110@163.com>
2017-06-20 11:34:26 +0800

    fixed:log

--- src/util/Log.php
+++ src/util/Log.php
@@ -175,7 +175,7 @@ final class Log {
         try {
             //$result=error_log($message, 3, $logFilePath);
             if(function_exists('swoole_async_write')){
-                $result = swoole_async_write($logFilePath, $message);
+                $result = swoole_async_write($logFilePath, $message."\t[swoole]");
             } else{
                 $result=file_put_contents($logFilePath, $message, FILE_APPEND);
             }
